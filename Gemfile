0138186b6cd4c10aec95337dfaa21a7b19492550
yamayamaKo <yamachanwow@gmail.com>
2021-04-09 07:46:48 +0900

    add 12章終わり letter_openerでメールが見れる

--- Gemfile
+++ Gemfile
@@ -24,6 +24,7 @@ group :development do
   gem 'listen',                '>= 3.1.5'
   gem 'spring',                '>= 2.0.2'
   gem 'spring-watcher-listen', '>= 2.0.1'
+  gem 'letter_opener_web'
 end
 
 group :test do
--- config/environments/development.rb
+++ config/environments/development.rb
@@ -28,7 +28,7 @@ Rails.application.configure do
 
   # Don't care if the mailer can't send.
   config.action_mailer.raise_delivery_errors = true
-  config.action_mailer.delivery_method = :test
+  config.action_mailer.delivery_method = :letter_opener_web
   host = 'localhost:3000' # ここをコピペすると失敗します。自分の環境に合わせてください。
   config.action_mailer.default_url_options = { host: host, protocol: 'http' }
 
