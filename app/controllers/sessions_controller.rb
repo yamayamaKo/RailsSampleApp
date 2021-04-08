5cb3a4297a89f5db55f98ea343a3ebcf123a1875
yamayamaKo <yamachanwow@gmail.com>
2021-04-08 09:04:43 +0900

    add 10章終わり

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -7,7 +7,7 @@ class SessionsController < ApplicationController
     if user && user.authenticate(params[:session][:password])
       # ユーザーログイン後にユーザー情報のページへ飛ぶ
       log_in user 
-      redirect_to user 
+      redirect_back_or user 
     else
       flash.now[:danger] = 'Invalid email/password combination'
       render 'new'
